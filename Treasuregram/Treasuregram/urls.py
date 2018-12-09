5eabdd865e1f67b68b883a5ad3750c0b8093fa91
shutiao <xuhaihua90@gmail.com>
2018-12-09 19:42:23 +0800

    Create Index View

--- Treasuregram/Treasuregram/urls.py
+++ Treasuregram/Treasuregram/urls.py
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from main_app import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    # localhost/index
+    path('index/', views.index),
 ]
