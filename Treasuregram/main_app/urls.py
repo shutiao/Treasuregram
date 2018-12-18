d204291946085be91d2d5aa0217245ca453ecd59
Haihua Xu <harrison_xu@sphinx.work>
2018-12-18 23:42:06 +0800

    Feat:Create a Detail Page

--- Treasuregram/main_app/urls.py
+++ Treasuregram/main_app/urls.py
@@ -1,6 +1,7 @@
-from django.urls import path
+from django.urls import path, re_path
 from . import views
 
 urlpatterns = [
         path('', views.index),
+        re_path(r'^([0-9]+)/$', views.detail, name = 'detail'),
         ]
--- Treasuregram/main_app/views.py
+++ Treasuregram/main_app/views.py
@@ -6,3 +6,7 @@ def index(request):
     # Here we need to get all of Treasure's objects
     treasures = Treasure.objects.all()
     return render(request, 'index.html', {'treasures': treasures})
+
+def detail(request, treasure_id):
+    treasure = Treasure.objects.get(id = treasure_id)
+    return render(request, 'detail.html', {'treasure': treasure})
