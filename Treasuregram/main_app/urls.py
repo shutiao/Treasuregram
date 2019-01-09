3943dda78595526558de09127387c3a51fcd37e0
Haihua Xu <harrison_xu@sphinx.work>
2019-01-09 19:19:28 +0800

    Feat: Set Up ImageField
    Level 3 Image Uploads - The Image Field

--- Treasuregram/main_app/urls.py
+++ Treasuregram/main_app/urls.py
@@ -1,8 +1,16 @@
 from django.urls import path, re_path
+from django.conf import settings
+from django.views.static import serve
 from . import views
 
 urlpatterns = [
         path('', views.index),
         re_path(r'^([0-9]+)/$', views.detail, name = 'detail'),
         path('post_url/', views.post_treasure, name='post_treasure'),
+        ]
+
+if settings.DEBUG:
+        urlpatterns += [
+                re_path(r'^media/(?P<path>.*)$', serve,
+                {'document_root': settings.MEDIA_ROOT,}),
         ]
